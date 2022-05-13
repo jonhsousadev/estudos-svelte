e1f42134a7c7f55b00b59fa6df8d502acf3e0fb5
Jonh Sousa <jonhnbsousa@gmail.comh>
2022-05-13 20:48:56 -0300

    feat: declaração reativa - exemplo de função reativa

--- exercicios/src/exercicios/Exercicio4.svelte
+++ exercicios/src/exercicios/Exercicio4.svelte
@@ -1,10 +1,23 @@
 <script>
   export let primeiroNome;
   export let sobrenome;
+  let count = 0
+  
+  // Declarações reativas
   $: nomeCompleto = primeiroNome+' '+sobrenome
+  $: {
+    console.log(`O valor de count agora é: ${count}`)
+  }
+
+  function somarUm() {
+    count++
+  }
 
 </script>
 
 <div>
   Seu primeiro nome é: {primeiroNome} e seu sobrenome é: {sobrenome}, logo seu nome completo é: {nomeCompleto}
+  <button on:click={somarUm}>
+    somar 1: {count}
+  </button>
 </div>
