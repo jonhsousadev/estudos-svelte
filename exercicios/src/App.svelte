3cc5018e78ce4b1deab83e0b48c8b4206e68f1ca
Jonh Sousa <jonhnbsousa@gmail.comh>
2022-05-13 20:43:12 -0300

    feat: declaração reativa - exemplo do nome completo

--- exercicios/src/App.svelte
+++ exercicios/src/App.svelte
@@ -2,6 +2,7 @@
 	import Exercicio1 from './exercicios/Exercicio1.svelte'
 	import Exercicio2 from './exercicios/Exercicio2.svelte'
 	import Exercicio3 from './exercicios/Exercicio3.svelte'
+	import Exercicio4 from './exercicios/Exercicio4.svelte'
 	export let name;
 </script>
 
@@ -13,6 +14,8 @@
 	<Exercicio2></Exercicio2>
 	<p>Executando exercício 3:</p>
 	<Exercicio3 value1="5" value2="2"></Exercicio3>
+	<p>Executando exercício 4:</p>
+	<Exercicio4 primeiroNome="Jonh" sobrenome="Sousa"></Exercicio4>
 	<hr>
 </main>
 
---
+++ exercicios/src/exercicios/Exercicio4.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let primeiroNome;
+  export let sobrenome;
+  $: nomeCompleto = primeiroNome+' '+sobrenome
+
+</script>
+
+<div>
+  Seu primeiro nome é: {primeiroNome} e seu sobrenome é: {sobrenome}, logo seu nome completo é: {nomeCompleto}
+</div>
