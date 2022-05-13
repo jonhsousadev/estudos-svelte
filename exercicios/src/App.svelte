2d86f255d8637e914df189b3aaea51ca7385ee50
Jonh Sousa <jonhnbsousa@gmail.comh>
2022-05-13 19:39:08 -0300

    feat - Exercício 2

--- exercicios/src/App.svelte
+++ exercicios/src/App.svelte
@@ -1,11 +1,15 @@
 <script>
 	import Exercicio1 from './exercicios/Exercicio1.svelte'
+	import Exercicio2 from './exercicios/Exercicio2.svelte'
 	export let name;
 </script>
 
 <main>
 	<p>Executando exercício 1:</p>
 	<Exercicio1 {name}></Exercicio1>
+	<hr>
+	<p>Executando exercício 1:</p>
+	<Exercicio2></Exercicio2>
 	<hr>
 </main>
 
@@ -14,6 +18,5 @@
 		text-align: center;
 		padding: 1em;
 		margin: 0 auto;
-		
 	}
 </style>
---
+++ exercicios/src/exercicios/Exercicio2.svelte
@@ -0,0 +1,9 @@
+<script>
+  let html = `This paragraph contains a strong text: <strong>TEXT</strong>`
+</script>
+
+<div>
+  {html}
+  <br>
+  {@html html}
+</div>
